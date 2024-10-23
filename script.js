405d0c304626a035f7c695d83f08b0b0dcc5c4e0
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-10-23 17:35:03 +0200

    feat: write convertToRoman function

--- script.js
+++ script.js
@@ -14,14 +14,98 @@ const roman = {
   IV: 4,
   I: 1,
 };
+let userNumber = document.querySelector("#user-number").value;
 
-function convertToRoman(number) {
-  let numArray = number.toString().split("");
-  console.log(numArray);
-  let ones = numArray[numArray.length - 1];
-  let tens = numArray[numArray.length - 2];
-  let hundreds = numArray[numArray.length - 3];
-  let thousands = numArray[numArray.length - 4];
+function getKeyByValue(object, value) {
+  const values = Object.values(object);
+  const index = values.indexOf(value);
+  if (index !== -1) {
+    const keys = Object.keys(object);
+    return keys[index];
+  }
+  return null;
 }
 
-convertToRoman(1234);
+function convertToRoman(number) {
+  if (number >= 4000) {
+    return false;
+  } else {
+    let numArray = number.toString().split("");
+    let ones = Number(numArray[numArray.length - 1]);
+    let tensDigit = Number(numArray[numArray.length - 2]);
+    let tens = Number(numArray[numArray.length - 2] * 10);
+    let hundredsDigit = Number(numArray[numArray.length - 3]);
+    let hundreds = Number(numArray[numArray.length - 3] * 100);
+    let thousandsDigit = Number(numArray[numArray.length - 4]);
+    let thousands = Number(numArray[numArray.length - 4] * 1000);
+
+    let romanOnes = getKeyByValue(roman, ones);
+    let romanTens = getKeyByValue(roman, tens);
+    let romanHundreds = getKeyByValue(roman, hundreds);
+    let romanThousands = getKeyByValue(roman, thousands);
+    let romanNumeral = [];
+    if (romanOnes !== null) {
+      romanNumeral.unshift(romanOnes);
+    } else {
+      romanOnes = "";
+      if (ones < 4) {
+        for (let i = 1; i <= ones; i++) {
+          romanOnes += "I";
+        }
+      } else if (romanOnes > 5 && romanOnes < 9) {
+        romanOnes = "V";
+        for (let i = 5; i <= romanOnes; i++) {
+          romanOnes += "I";
+        }
+      }
+      romanNumeral.unshift(romanOnes);
+    }
+    if (romanTens != null) {
+      romanNumeral.unshift(romanTens);
+    } else {
+      romanTens = "";
+      if (tensDigit < 4) {
+        for (let i = 1; i <= tensDigit; i++) {
+          romanTens += "X";
+        }
+      } else if (tensDigit > 5 && tensDigit < 9) {
+        romanTens = "L";
+        for (let i = 5; i <= tensDigit; i++) {
+          romanTens += "X";
+        }
+      }
+      romanNumeral.unshift(romanTens);
+    }
+    if (romanHundreds != null) {
+      romanNumeral.unshift(romanHundreds);
+    } else {
+      romanHundreds = "";
+      if (hundredsDigit < 4) {
+        for (let i = 1; i <= hundredsDigit; i++) {
+          romanHundreds += "C";
+        }
+      } else if (hundredsDigit > 5 && hundredsDigit < 9) {
+        romanHundreds = "D";
+        for (let i = 5; i <= hundredsDigit; i++) {
+          romanHundreds += "C";
+        }
+      }
+      romanNumeral.unshift(romanHundreds);
+    }
+    if (romanThousands != null) {
+      romanNumeral.unshift(romanThousands);
+    } else {
+      romanThousands = "";
+      if (thousandsDigit < 4) {
+        for (let i = 1; i <= thousandsDigit; i++) {
+          romanThousands += "M";
+        }
+      }
+      romanNumeral.unshift(romanThousands);
+    }
+
+    console.log(romanNumeral.toString().replaceAll(",", ""));
+  }
+}
+convertToRoman(2863);
+//convertToRoman(userNumber);

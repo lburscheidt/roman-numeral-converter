7e92c3bd254b6aee88e3a111334b6c5b38a6454e
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-10-23 16:14:09 +0200

    feat: add roman numeral object

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 console.log("hello");
+const roman = {
+  M: 1000,
+  CM: 900,
+  D: 500,
+  CD: 400,
+  C: 100,
+  XC: 90,
+  L: 50,
+  XL: 40,
+  X: 10,
+  IX: 9,
+  V: 5,
+  IV: 4,
+  I: 1,
+};
 
+function convertToRoman(number) {
+  let numArray = number.toString().split("");
+  console.log(numArray);
+  let ones = numArray[numArray.length - 1];
+  let tens = numArray[numArray.length - 2];
+  let hundreds = numArray[numArray.length - 3];
+  let thousands = numArray[numArray.length - 4];
+}
+
+convertToRoman(1234);
